8a1d1a0a886ff2932482654d47149776e25eeb42
Tsubasa Ozawa <t.ozawa.sake@gmail.com>
2022-01-22 19:02:04 +0900

    session

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
-	"strconv"
 
-	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/gommon/log"
 )
 
 // --------
@@ -15,35 +15,9 @@ import (
 
 // User ...
 type User struct {
-	ID   uint   `json:"id" param:"id"`
-	Name string `json:"name" validate:"required,yamada"` // validateタグ
-	Age  int    `json:"age"`
-	// SomethingArray []int  `json:"array" query:"array"`                   // queryタグ
-}
-
-// --------
-// validator↓
-// --------
-
-// CustomValidator ...
-type CustomValidator struct {
-	validator *validator.Validate
-}
-
-// Validate ...
-func (cv *CustomValidator) Validate(i interface{}) error {
-	cv.validator.RegisterValidation("yamada", CustomValidate)
-	return cv.validator.Struct(i)
-}
-
-// CustomValidate ...
-func CustomValidate(fl validator.FieldLevel) bool {
-	return fl.Field().String() == "yamada"
-}
-
-// NewCustomValidator ...
-func NewCustomValidator() echo.Validator {
-	return &CustomValidator{validator: validator.New()}
+    ID   uint   `json:"id"`
+    Name string `json:"name"`
+    Age  int    `json:"age"`
 }
 
 // --------
@@ -52,142 +26,69 @@ func NewCustomValidator() echo.Validator {
 
 // InitRouting ...
 func InitRouting(e *echo.Echo, u *User) {
-	e.POST("user", u.CreateUser)
-	e.PUT("user/:id", u.UpdateUser)
-	e.DELETE("user/:id", u.DeleteUser)
-	e.GET("user/:id", u.GetUser)
+    e.GET("login", u.Login)
 	e.GET("users", u.GetUsers)
+}
+
+// --------
+// middleware↓
+// --------
+
+func Middleware(e *echo.Echo) {
+	e.Use(session.Middleware(sessions.NewCookieStore([]byte("secret"))))
 }
 
 // --------
 // handler↓
 // --------
 
-// CreateUser ...
-func (u *User) CreateUser(c echo.Context) error {
-	user := User{}
-
-	if err := c.Bind(&user); err != nil {
-		log.Error(err)
-		return err
-	}
-	// バリデーションの呼び出し
-	if err := c.Validate(&user); err != nil {
-		log.Error(err)
-		return err
+// Login ...
+func (u *User) Login(c echo.Context) error {
+	// get new session
+    sess, err := session.Get("session_name", c)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, "failed to session.Get()")
 	}
 
-	user = User{
-		ID:   1,
-		Name: user.Name,
-		Age:  user.Age,
-	}
-
-	return c.JSON(http.StatusOK, user)
-}
-
-// UpdateUser ...
-func (u *User) UpdateUser(c echo.Context) error {
-	user := User{}
-
-	if err := c.Bind(&user); err != nil {
-		return err
-	}
-
-	return c.JSON(http.StatusOK, "Updated")
-}
-
-// DeleteUser ...
-func (u *User) DeleteUser(c echo.Context) error {
-	if err := c.Bind(u); err != nil {
-		return err
-	}
-	return c.JSON(http.StatusOK, "Deleted")
-}
-
-// GetUser ...
-func (u *User) GetUser(c echo.Context) error {
-	user := User{}
-
-	if err := c.Bind(&user); err != nil {
-		return err
-	}
-
-	// fmt.Println(user.ID)
-	// fmt.Println(user.Name)
-	// fmt.Println(user.Age)
-	// fmt.Println(user.SomethingArray)
-	// for _, v := range user.SomethingArray {
-	// 	fmt.Println(v)
+	// if !sess.IsNew {
+	// 	// session発行済みの場合はログイン済みとしてトップページにリダイレクト
+	// 	return c.Redirect(http.StatusOK, "/top_page")
 	// }
 
-	id, err := strconv.Atoi(c.Param("id"))
-	if err != nil {
-		return err
+	// set options
+    sess.Options = &sessions.Options{
+        Path:     "/",
+        MaxAge:   86400 * 7,
+        HttpOnly: true,
+    }
+
+	// set values
+	sess.Values["token"] = "xxx"
+
+	// save session
+    if err := sess.Save(c.Request(), c.Response()); err != nil {
+		return c.JSON(http.StatusInternalServerError, "failed to sess.Save()")
 	}
 
-	// Getメソッドのイメージ
-	if id == 1 {
-		user = User{
-			ID:   1,
-			Name: "Tom",
-			Age:  29,
-		}
-	} else if id == 2 {
-		user = User{
-			ID:   2,
-			Name: "Bob",
-			Age:  35,
-		}
-
-	} else {
-		return c.JSON(http.StatusOK, "Not Found")
-	}
-
-	return c.JSON(http.StatusOK, user)
+    return c.Redirect(http.StatusFound, "users")
 }
 
-// GetUsers ...
-func (u *User) GetUsers(c echo.Context) error {
-	users := []*User{}
-
-	name := c.QueryParam("name")
-
-	// Get Allメソッドのイメージ
-	if name == "" {
-		users = []*User{
-			{
-				ID:   1,
-				Name: "Tom",
-				Age:  29,
-			},
-			{
-				ID:   2,
-				Name: "Bob",
-				Age:  35,
-			},
-		}
-	} else if name == "Tom" {
-		users = []*User{
-			{
-				ID:   1,
-				Name: "Tom",
-				Age:  29,
-			},
-		}
-	} else if name == "Bob" {
-		users = []*User{
-			{
-				ID:   2,
-				Name: "Bob",
-				Age:  35,
-			},
-		}
-	} else {
-		return c.JSON(http.StatusOK, "Not Found")
+func(u *User) GetUsers(c echo.Context) error {
+	sess, err := session.Get("session_name", c)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, "failed to session.Get()")
+	}
+	if sess.IsNew {
+		log.Printf("%+v", *sess)
+		return c.JSON(http.StatusOK, "Can't Get Users, because session is new")
 	}
 
-	return c.JSON(http.StatusOK, users)
+	user := User{
+		ID: 1,
+		Name: "Yamada",
+		Age: 30,
+	}
+	return c.JSON(http.StatusOK, user)
 }
 
 // --------
@@ -195,13 +96,12 @@ func (u *User) GetUsers(c echo.Context) error {
 // --------
 
 func main() {
-	e := echo.New()
+    e := echo.New()
 
-	// CustomValidatorのインスタンス生成
-	e.Validator = NewCustomValidator()
+    Middleware(e)
 
-	u := new(User)
-	InitRouting(e, u)
+    u := new(User)
+    InitRouting(e, u)
 
-	e.Start(":9005")
+    e.Start(":9005")
 }
